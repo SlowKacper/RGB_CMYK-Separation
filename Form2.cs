1bc6ba634117dda83f16547358b2bb1e424944c5
SlowKacper <60720365+SlowKacper@users.noreply.github.com>
2020-02-06 23:25:05 +0100

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proj3
+{
+    public partial class Form2 : Form
+    {
+        public Form2(Bitmap Magenta, Bitmap Yellow, Bitmap Cyan, Bitmap Black)
+        {
+
+            InitializeComponent();
+            Magenta = new Bitmap(Magenta, pictureBox2.Width, pictureBox2.Height);
+            Yellow = new Bitmap(Yellow, pictureBox3.Width, pictureBox3.Height);
+            Cyan = new Bitmap(Cyan, pictureBox1.Width, pictureBox1.Height);
+            Black = new Bitmap(Black, pictureBox4.Width, pictureBox4.Height);
+            pictureBox1.Image = Cyan;
+            pictureBox2.Image = Magenta;
+            pictureBox3.Image = Yellow;
+            pictureBox4.Image = Black;
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,30 @@
+-Przesuwanie krwędzi następuje po wyborze krawędzi Radiobuttonem z jej nazwą i przesunięciem punktów kontrolnych.
+
+- UCR, GCR, 100% cofnięcia, 0% cofnięcia
+tworzą domyślne krzywe.
+
+-Load Image
+Ładuje zdjędzie w formacie jpg do pictureboxa.
+
+- Load Curve: ładuje krzywe z plik txt w formacie:
+X/Y/X/Y/X/Y/X/Y
+X/Y/X/Y/X/Y/X/Y
+X/Y/X/Y/X/Y/X/Y
+X/Y/X/Y/X/Y/X/Y
+Cyan
+Magenta
+Yellow
+Black
+
+- Save Curve: zapisuje krzywe do pliku txt w formacie:
+X/Y/X/Y/X/Y/X/Y
+X/Y/X/Y/X/Y/X/Y
+X/Y/X/Y/X/Y/X/Y
+X/Y/X/Y/X/Y/X/Y
+Cyan
+Magenta
+Yellow
+Black
+
+- Save pictures:
+zapisuje pliki z przyrostkiem od danego koloru: _Cyan, _Magenta, _Yellow, _Black
